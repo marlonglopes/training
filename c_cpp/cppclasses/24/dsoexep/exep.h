8eea1cb8da7cdc679ec8a49c256637dba1f55f0f
Mateus  Vendramini <mateusvendramini21@gmail.com>
2015-08-14 11:34:39 -0300

    better now

--- c_cpp/cppclasses/24/dsoexep/exep.h
+++ c_cpp/cppclasses/24/dsoexep/exep.h
@@ -2,13 +2,17 @@
 #ifndef __lib_exep_h__
 #define __lib_exep_h__
 
+#ifdef __cplusplus
 extern "C" {
+#endif
 
   void hello();
 
   // Aqui nao da pra usar templates, funcoes sobrecarregadas e qualquer coisa que seja cpp-only
 
+#ifdef __cplusplus
 }
+#endif 
 
 #endif // __lib_exep_h__
 
