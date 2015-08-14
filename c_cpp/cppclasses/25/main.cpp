2a8e1df45d8e73a9c5590093423478154306cfb4
Mateus  Vendramini <mateusvendramini21@gmail.com>
2015-08-14 14:11:25 -0300

    adding doc commentary, study class, soname

--- c_cpp/cppclasses/25/main.cpp
+++ c_cpp/cppclasses/25/main.cpp
@@ -6,6 +6,14 @@
 
 /*
 Bibliotecas dinamicas (DSOs)
+
+Quando linkamos com alguma lib, o linker le a o soname da lib
+Esta string entao e usada pra definir a linkagem, ao inves do filename
+
+Isto serve para "influenciar" os binarios que dependam da lib, dependentes
+de uma "PK". No futuro, e possivel deployar novas versoes dessa
+lib, e os binarios vao cotninuar funcionando, porque vao procurar a mesma
+"PK" pra decidir se a linkagem esta satisfeita ou nao.
 */
 
 void func(){
